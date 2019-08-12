02919d8153d67a8f84dbc3266bb8ab76a759fdc7
AlphaCodingcode <dilshan@alphacoding.com>
2019-08-12 15:05:21 -0400

    game over

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let world;
+let player;
+let GAMEOVER = false;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     world = new Backdrop(color(20, 20, 20), 5, 10);
-    console.log(world);
+    player = new Ship(100, 100, world);
 }
 
 /*
@@ -12,8 +14,16 @@ function setup() {
 function draw() {
     // Update
     world.update();
+    player.update();
     // Render
     world.render();
+    player.render();
+
+    if (GAMEOVER) {
+        gameOver();
+        noLoop();
+    }
+
 }
 
 
@@ -24,4 +34,12 @@ function mouseClicked() {
     else
         loop();
     state = !state;
+}
+
+
+function gameOver() {
+    fill(255);
+    textAlign(CENTER);
+    textSize(50);
+    text("GAME OVER!", width / 2, height / 2);
 }
