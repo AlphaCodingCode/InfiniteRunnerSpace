f7b41d5bb7c4c1aca484ee37f7d7ce74c99371f1
AlphaCodingcode <dilshan@alphacoding.com>
2019-08-12 16:10:43 -0400

    meteor obstacle generation

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 let world;
 let player;
+let meteors = [];
 let GAMEOVER = false;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     world = new Backdrop(color(20, 20, 20), 5, 10);
-    player = new Ship(100, 100, world);
+    player = new Ship(world);
+    meteors.push(new Meteor(width + 300, random(height / 4, 3 * height / 4), random(60, 160), world));
+    meteors.push(new Meteor(width + 900, random(height / 4, 3 * height / 4), random(60, 160), world));
 }
 
 /*
@@ -15,9 +18,22 @@ function draw() {
     // Update
     world.update();
     player.update();
+
+    for (let i = meteors.length - 1; i >= 0; i--) {
+        meteors[i].update();
+        if (meteors[i].x < 0 - meteors[i].size) {
+            meteors.push(new Meteor(width + random(180, 350), random(height / 4, 3 * height / 4), random(60, 160), world));
+            meteors.splice(i, 1);
+        }
+    }
+
+
     // Render
     world.render();
     player.render();
+    for (let i = meteors.length - 1; i >= 0; i--) {
+        meteors[i].render();
+    }
 
     if (GAMEOVER) {
         gameOver();
